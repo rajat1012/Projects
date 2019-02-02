be87ad6f5666fb339cf8d1565e1ca085434e5a32
Rajat Acharya <juiashinkar@JUIs-MacBook-Pro.local>
2019-02-01 19:01:28 -0500

    Implementin Functionaliy

--- IOS/Quizzler/Quizzler/ViewController.swift
+++ IOS/Quizzler/Quizzler/ViewController.swift
@@ -2,17 +2,17 @@
 //  ViewController.swift
 //  Quizzler
 //
-//  Created by Angela Yu on 25/08/2015.
-//  Copyright (c) 2015 London App Brewery. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
     
-    //Place your instance variables here
+    // instance variables 
     
-    
+    let allQuestions = QuestionBank()
+    var pickedAnswer :Bool = false
+    var questionNumber:Int = 0
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet var progressBar: UIView!
@@ -20,12 +20,22 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        questionLabel.text = allQuestions.list[questionNumber].questionText
+       
         
     }
 
 
     @IBAction func answerPressed(_ sender: AnyObject) {
-  
+        if sender.tag == 1 {
+           pickedAnswer = true
+        }
+        else if sender.tag == 2{
+           pickedAnswer = false
+        }
+        checkAnswer()
+        questionNumber = questionNumber + 1
+        nextQuestion()
     }
     
     
@@ -36,11 +46,24 @@ class ViewController: UIViewController {
 
     func nextQuestion() {
         
+        if questionNumber <= 12 {
+            questionLabel.text = allQuestions.list[questionNumber].questionText
+        }
+        else{
+            print("End of Quiz")
+            questionNumber = 0
+        }
+        
     }
     
-    
     func checkAnswer() {
-        
+        let correctAnswer = allQuestions.list[questionNumber].answer
+        if correctAnswer == pickedAnswer {
+            print("You Got it right")
+        }
+        else {
+            print("wrong")
+        }
     }
     
     
